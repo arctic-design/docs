0a19541979327213da10b7f7761400622542bff1
barunprasad <barun.prasad@hotmail.com>
2024-12-27 11:53:37 +0000

    Deploying to gh-pages from @ arctic-design/docs@522054d75ed2254e64b52967486342bab2699df0 🚀

---
+++ _next/static/URAa8Kpwct_DtipqvBLUN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-ffc8615701a084a9.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
